b919a4df195d6169a1e3ecdb3319b9cc6fc06d18
Soulskater <gmeszaros007@gmail.com>
2013-10-05 11:00:30 +0200

    Modified server.py

--- RobotSoftware/Communication/Server.py
+++ RobotSoftware/Communication/Server.py
@@ -12,7 +12,7 @@ def run():
 
     #ip and port of servr
     #by default http server port is 80
-    ip = socket.gethostbyname(socket.gethostname())
+    ip = socket.getfqdn()
     server_address = (ip, 80)
     httpd = HTTPServer(server_address, RobotHTTPRequestHandler)
     print('http server is running on %s ...' % ip)
