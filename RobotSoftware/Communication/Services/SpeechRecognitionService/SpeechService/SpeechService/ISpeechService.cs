1cddfbb6574961fd1f3f92eaf155aa940bc8109e
Soulskater <gmeszaros007@gmail.com>
2013-12-11 09:40:12 +0100

    abc

---
+++ RobotSoftware/Communication/Services/SpeechRecognitionService/SpeechService/SpeechService/ISpeechService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SpeechService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ISpeechService
+    {
+        [OperationContract]
+        string RecognizeInput(Stream input);
+    }
+}
---
+++ RobotSoftware/Communication/Services/SpeechRecognitionService/SpeechService/SpeechService/SpeechGrammar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechService
+{
+    public static class SpeechGrammar
+    {
+        private static Choices commands = new Choices(new string[] {"helloworld", "hi", "there", "hi there" ,"forward", "backward", "left", "right" });
+        public static Grammar Grammar;
+
+        public static void LoadGrammar()
+        {
+            if (Grammar != null) return;
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(commands);
+
+            // Create the Grammar instance.
+            Grammar = new Grammar(gb);
+        }
+    }
+}
